ddbf3ab6f4bba3c3a87bf37d0e0cf43ed195cbd8
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    "Initial commit"

---
+++ index.js
@@ -0,0 +1,44 @@
+// Require the framework and instantiate it
+const fastify = require("fastify")({
+  logger: true,
+});
+
+// Declare a route
+//fastify.get("/", const handler = (request, reply) =>
+// reply.send({ hello: "world" });
+
+fastify.get("/", (request, reply) => {
+  reply.send({ hello: "world" });
+});
+
+fastify.get("/api/healthchecker", (request, reply) => {
+  reply.status(200).send({
+    //reply.status(200).send({
+    status: "success",
+    message: "Build CRUD API with Node.js and Sequelize",
+  });
+});
+fastify.get("/api/healthchecker", (request, reply) => {
+  reply.status(200).send({
+    status: "success",
+    message: "Build CRUD API with Node.js and Sequelize",
+  });
+});
+
+fastify.all("*", (request, reply) => {
+  reply.status(404).send({
+    status: "fail",
+   // message: "Route does not exist on this server",
+  // message: `Route: ${request.raw.url} does not exist on this server`,
+    message: `Route: ${request.raw.url} does not exist on this server`,
+  });
+});
+// Run the server!
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  //console.log("server is now listening on ${address}");
+  fastify.log.info(`server listening on ${address}`);
+});
